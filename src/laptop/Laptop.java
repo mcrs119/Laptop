35a47f16eed4c9c9a00cd463b7042dcc084065d8
mcrs19 <balthru119@gmail.com>
2022-04-13 14:43:18 +0900

    cb

--- src/laptop/Laptop.java
+++ src/laptop/Laptop.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 
 public class Laptop {
-
+public static void main(String[] args) {
+		
+	
+	
 		//1.노트북 스펙 -스펙 크기.무게로작성하고  - 클래스(Spec) - 승대
 		
 		//2. 가격 - 30만원부터 180만원까지 설정 - 클래스 (Price) - 주남
@@ -12,4 +15,5 @@ public class Laptop {
 		//3. 제조사 - 4군데 - 삼성. 엘지 . 아수스 . 레노버 - 클래스(Sn) - 병원
 
 		//메인 - 스펙 가격 제조사를 입력해서 원하는 결과물 찾기 
+}
 }
---
+++ src/laptop/SpecDTO.java
@@ -0,0 +1,22 @@
+package laptop;
+
+public class SpecDTO {
+
+	public int size;
+	public int weigth;
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public int getWeigth() {
+		return weigth;
+	}
+	public void setWeigth(int weigth) {
+		this.weigth = weigth;
+	}
+	
+	
+}
