26e76245201f40a3d24fc37d4ed3914d8f7616fd
junam <a@a.a>
2022-04-13 14:57:24 +0900

    저장

---
+++ src/laptop/Laptop.java
@@ -0,0 +1,47 @@
+package laptop;
+
+import java.util.ArrayList;
+
+class price{
+	private String price;
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public void puint() {
+		while(true) {
+			System.out.println(" 가격 ");
+			
+		}
+		
+		
+		
+	}
+	
+}
+
+public class Laptop {
+	public static void main(String[] args) {
+		//1.노트북 스펙 -스펙 크기.무게로작성하고  - 클래스(Spec) - 승대
+		
+		//2. 가격 - 30만원부터 180만원까지 설정 - 클래스 (Price) - 주남
+				
+		//3. 제조사 - 4군데 - 삼성. 엘지 . 아수스 . 레노버 - 클래스(Sn) - 병원
+
+		//메인 - 스펙 가격 제조사를 입력해서 원하는 결과물 찾기 
+			
+			
+			
+		
+		
+	}
+
+		
+	
+	
+	
+}
